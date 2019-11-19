84032db240f38a7ca5b955cd21e21438de198815
Eightaan <57832730+Eightaan@users.noreply.github.com>
2019-11-19 14:27:58 +0100

    Update interactionext.lua

--- Loot info text/interactionext.lua
+++ Loot info text/interactionext.lua
@@ -3,7 +3,7 @@ LocalizationManager:add_localized_strings({["hud_int_hold_grab_the_bag"] = "Hold
 local _add_string_macros_original = BaseInteractionExt._add_string_macros
 function BaseInteractionExt:_add_string_macros(macros, ...)
 	_add_string_macros_original(self, macros, ...)
-	macros.BTN_INTERACT = macros.BTN_INTERACT or managers.localization:get_default_macro("BTN_INTERACT") --Ascii ID for RB
+	macros.INTERACT = self:_btn_interact() or managers.localization:get_default_macro("BTN_INTERACT") --Ascii ID for RB
 	if self._unit:carry_data() then
 		local carry_id = self._unit:carry_data():carry_id()
 		macros.BAG = managers.localization:text(tweak_data.carry[carry_id].name_id)
